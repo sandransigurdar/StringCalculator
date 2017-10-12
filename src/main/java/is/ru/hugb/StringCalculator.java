47697db0126fabf3ca01cf6def2acd2a0c6e8393
sandransigurdar <sandranordfjord11@gmail.com>
2017-10-12 19:14:19 +0000

    Negative numbers og test

--- src/main/java/is/ru/hugb/StringCalculator.java
+++ src/main/java/is/ru/hugb/StringCalculator.java
@@ -3,11 +3,15 @@ package is.ru.StringCalculator;
 public class StringCalculator {
 	
 	public static int Add(String numbers) {
-		if(numbers.equals("")) {
+	    if(numbers.equals("")) {
             return 0;
         }
         else if(!numbers.contains(",") && !numbers.contains("\n")) {
-        	return Integer.parseInt(numbers);
+        	int num = Integer.parseInt(numbers);
+        	if(num < 0) {
+        		throw new IllegalArgumentException("Negatives not allowed: " + num);
+        	}
+            else return num;
         }
         else if(numbers.contains(",") || numbers.contains("\n")) {
             String nums[] = numbers.split(",|\n");
@@ -18,8 +22,13 @@ public class StringCalculator {
 
     private static int sum(String[] nums) {
         int sum = 0;
+        int i = 0;
         for(String num : nums) {
-            sum += Integer.parseInt(num);
+        	int no = Integer.parseInt(num);
+            sum += no;
+            if(no < 0) {
+            	throw new IllegalArgumentException("Negatives not allowed: " + no);
+            }
         }
         return sum;
     }
--- src/test/java/is/ru/hugb/StringCalculatorTest.java
+++ src/test/java/is/ru/hugb/StringCalculatorTest.java
@@ -29,4 +29,17 @@ public class StringCalculatorTest {
     public void testAddEnterComma() {
     	assertEquals(6, StringCalculator.Add("1\n2,3"));
     }
+
+    @Test
+    public void testNegatives() {
+    	//RuntimeException e = null;
+    	//ExpectedException thrown = null;
+    	try {
+    		StringCalculator.Add("-1,2");
+    	 	//assertEquals("Negatives not allowed: -1", StringCalculator.Add("-1"))
+    	}
+    	catch(RuntimeException e){
+    		assertEquals("Negatives not allowed: -1", e.getMessage());
+    	}
+    }
 }
